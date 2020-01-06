a20eb4bfde12c015e98e7e259f1ead4ed853aa17
ZivaUrbancic <ziva.urbancic@gmail.com>
2020-01-06 15:32:54 +0100

    Expanded documentation

--- DOCUMENTSIMILARITY/microservice/routes/service.py
+++ DOCUMENTSIMILARITY/microservice/routes/service.py
@@ -14,138 +14,169 @@ import requests
 from ..library.document_similarity import DocumentSimilarity
 from ..library.postgresql import PostgresQL
 
+#################################################
+# Get parameters from .config file
+#################################################
 
-# GET MODEL PARAMETERS FROM THE .config FILE
-# database with document texts
+# database parameters
 database_name = app.config['DATABASE_NAME']
 database_user = app.config['DATABASE_USER']
 database_password = app.config['DATABASE_PASSWORD']
 # url to text embedding service
 text_embedding_url = app.config['TEXT_EMBEDDING_URL']
 
-# Connect to the embeddings database and retrieve the embeddings:
+
+#################################################
+# Connect to the database
+#################################################
+
 pg = PostgresQL()
 pg.connect(database=database_name, user=database_user,
                       password=database_password)
 
 
 #################################################
-# Setup the embeddings blueprint
+# Setup the similarity blueprint
 #################################################
 
-# TODO: provide an appropriate route name and prefix
 bp = Blueprint('similarity', __name__, url_prefix='/api/v1/similarity')
 
+
+#################################################
+# Index route:
+#################################################
 
 @bp.route('/', methods=['GET'])
 def index():
     # TODO: provide an appropriate output
     return abort(501)
 
+
+#################################################
+# Route for updating the database
+#################################################
+
 @bp.route('/new_document_embedding', methods=['GET', 'POST'])
 def update_similarities():
     # TODO: write documentation
 
-    # get document id as the parameter
+
+    # Retrieve 'document_id' as a request parameter
+
     if request.method == 'GET':
-        # retrieve new document's id
         try:
-            doc_id = request.args.get('document_id', default=None, type=int)
+            document_id = request.args.get('document_id', default=None, type=int)
         except Exception as e:
-            return abort(401, e)
+            return abort(401, "Could not retrieve parameter 'document_id', method = 'GET'. " + str(e))
     elif request.method == 'POST':
         try:
-            # retrieve the text posted to the route
-            doc_id = request.json['document_id']
+            document_id = request.json['document_id']
         except Exception as e:
-            return abort(401, str(e))
+            return abort(401, "Could not retrieve parameter 'document_id', method = 'POST'. " + str(e))
     else:
-        # TODO: log exception
         return abort(405)
 
-    # get embeddings from postgres
-    # TODO: change the expression if needed
+
+    # Retrieve the embeddings from the database
+
     try:
+        # get embeddings from postgres
         loaded_embedding = pg.execute("""
             SELECT * FROM document_embeddings;
             """)
-
+        # Separate the result into a list of indices and a matrix of embeddings
         indices = [embedding['document_id'] for embedding in loaded_embedding]
         embeddings = [embedding['vector'] for embedding in loaded_embedding]
     except Exception as e:
-        return abort(404, str(e))
+        return abort(404, "Could not retrieve document embeddings from the database. " + str(e))
+
+
+    # Retrieve the document's text from the database
 
     try:
-        # retrieve the document's text
-        # TODO: change the expression if needed
-        try:
-            statement = """
-                SELECT fulltext_cleaned FROM documents
-                WHERE document_id={};
-                """.format(doc_id)
-            document_text = (pg.execute(statement))[0]['fulltext_cleaned']
-        except Exception as e:
-            return abort(400, "Could not retrieve text of the document from the database. "+str(e))
-
-        # Call the text-embedding-service and produce the embedding
-        # TODO: add language_model parameter
-        try:
-            params = {'text': document_text, 'language': 'en'}
-            service_response = requests.post(url=text_embedding_url, json=params).json()
-            new_embedding = service_response['embedding']
-        except Exception as e:
-            return abort(400, "Could not retrieve the embedding from the text embedding service. " + str(e))
-
-        # Using DocumentSimilarity compute similarities and return them
-        similarity = DocumentSimilarity(embedding=embeddings, indices=indices)
-        additional_similarities = similarity.new_document(doc_id, new_embedding)
-
-        # insert a new embedding into the database
-        try:
-            pg.execute("""
-                INSERT INTO document_embeddings
-                VALUES ({}, ARRAY{});
-                """.format(doc_id, new_embedding))
-            pg.commit()
-        except Exception as e:
-            return abort(400, "Could not add the new embedding into the table 'document_embeddings'. " + str(e))
-
-        # insert similarities into the database
-        try:
-            for i, j, sim in additional_similarities:
-                pg.execute("""
-                    INSERT INTO similarities
-                    VALUES ({}, {}, {});
-                    """.format(i, j, sim))
-                pg.commit()
-        except Exception as e:
-            return abort(400, "Could not add the additional similarities into the table 'similarities'. " + str(e))
-
-        finish = True
+        statement = """
+            SELECT fulltext_cleaned FROM documents
+            WHERE document_id={};
+            """.format(document_id)
+        document_text = (pg.execute(statement))[0]['fulltext_cleaned']
     except Exception as e:
-        # TODO: log exception
-        # something went wrong with the request
-        return abort(400, str(e))
-    else:
-        # TODO: return the response
-        # additional_similarities = "TODO"
-        return jsonify({
+        return abort(400, "Could not retrieve text of the document from the database. "+str(e))
+
+
+    # Construct the new document's embedding
+
+    # TODO: add language_model parameter
+    try:
+        # Call the text-embedding-service and produce the embedding
+        params = {'text': document_text, 'language': 'en'}
+        service_response = requests.post(url=text_embedding_url, json=params).json()
+        new_embedding = service_response['embedding']
+    except Exception as e:
+        return abort(400, "Could not retrieve the embedding from the text embedding service. " + str(e))
+
+
+    # Compute similarities with other documents
+
+    try:
+        # initialize the model
+        similarity = DocumentSimilarity(embedding=embeddings, indices=indices)
+        # retrieve the new similarities to be added to the table
+        additional_similarities = similarity.new_document(document_id, new_embedding)
+    except Exception as e:
+        return abort(400, "Could not retrieve similarities between documents. " + str(e))
+
+
+    # Insert the new embedding into the database
+
+    try:
+        pg.execute("""
+            INSERT INTO document_embeddings
+            VALUES ({}, ARRAY{});
+            """.format(document_id, new_embedding))
+        pg.commit()
+    except Exception as e:
+        return abort(400, "Could not add the new embedding into the table 'document_embeddings'. " + str(e))
+
+
+    # Insert similarities into the database
+
+    try:
+        for i, j, sim in additional_similarities:
+            pg.execute("""
+                INSERT INTO similarities
+                VALUES ({}, {}, {});
+                """.format(i, j, sim))
+            pg.commit()
+    except Exception as e:
+        return abort(400, "Could not add the additional similarities into the table 'similarities'. " + str(e))
+
+
+    # Return the result
+
+    finish = True
+    return jsonify({
             "embedding": new_embedding,
             "additional similarities": additional_similarities,
             "indices": indices,
             "finish": finish
         })
-            # "indices": indices,
+
+
+#################################################
+# Route for retrieving similar documents:
+#################################################
 
 @bp.route('/get_similarities', methods=['GET', 'POST'])
 def get_similarities():
     # TODO: write documentation
+
+
+    # Retrieve query parameters 'document_id' and 'get_k'
+
     if request.method == 'GET':
-        # retrieve the correct query parameters
         doc_id = request.args.get('document_id', default=None, type=int)
         k = request.args.get('get_k', default=None, type=int)
     elif request.method == 'POST':
-        # retrieve the text posted to the route
         doc_id = request.json['document_id']
         k = request.json['get_k']
     else:
