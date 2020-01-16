9091eac4c3ff020f5836f6ace1fc893e681a9833
Erik Novak <novak.erik@gmail.com>
2020-01-16 14:00:29 +0100

    Add pylint configuration and fix the print checking

--- scripts/bash_tests.sh
+++ scripts/bash_tests.sh
@@ -7,7 +7,7 @@
 testFilesWithPrint() {
     # count the number of print statements in the file
     # The number of print statements should be 0
-    FILES_WITH_PRINT=$(grep -Rwl --exclude-dir=static --exclude-dir=scripts "print" ./*)
+    FILES_WITH_PRINT=$(grep -Rwl --exclude-dir=static --exclude-dir=scripts --exclude-dir=__pycache__ --exclude-dir=venv "print" ./*)
     FILES_COUNT=$(echo "$FILES_WITH_PRINT" | wc -l)
 
     if [ $FILES_COUNT != 0 ]; then
