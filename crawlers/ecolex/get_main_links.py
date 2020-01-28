f3e20f3f507337132ea9a09ec8fa103717ad391c
KraljSamo <samokralj95@gmail.com>
2020-01-28 14:38:55 +0100

    updated script to get document links

--- crawlers/ecolex/get_main_links.py
+++ crawlers/ecolex/get_main_links.py
@@ -6,11 +6,14 @@ def get_main_links(filterSLO=True):
     """
     Function that will extract all the links to documents from 'ecolex.org' and save them into a file.
 
-    :filterSLO:     Set to True we will extract links only for documents that are relevant to Slovenia
-                    and save them into file 'main_links_SLO.txt'
-                    Set to False we will extract all the links and save them into file 'main_links_ALL.txt'
-
-    returns None
+    Parameters:
+        filterSLO : boolean
+                Set to True we will extract links only for documents that are relevant to Slovenia
+                and save them into file 'main_links_SLO.txt'
+                Set to False we will extract all the links and save them into file 'main_links_ALL.txt'
+    
+    Returns 
+        None
     """
 
     # Link to the documents that are relevant for Slovenia, currently there are around 620 pages.
@@ -32,30 +35,27 @@ def get_main_links(filterSLO=True):
         pages = 10640
         link = LINK_ALL
 
-    file_links = open(filename, 'w')
+    with open(filename, 'w') as file_links:
 
-    reLink = re.compile(r'search-result-title">\s*<a href="(.*?)"')
+        regex_link = re.compile(r'search-result-title">\s*<a href="(.*?)"')
 
-    start_time = time()
+        start_time = time()
 
-    for page in range(1, pages + 1):
-        page_link = link + str(page)
+        for page in range(1, pages + 1):
+            page_link = link + str(page)
 
-        openpage = requests.get(url=page_link)
-        html_text = openpage.text
+            open_page = requests.get(url=page_link)
+            html_text = open_page.text
 
-        hits = re.findall(reLink, html_text)
+            hits = re.findall(regex_link, html_text)
 
-        for hit in hits:
-            file_links.write(hit + '\n')
+            for hit in hits:
+                file_links.write(hit + '\n')
 
-        if page % 20 == 0:
-            print(page, round(time() - start_time, 6))
-
-    file_links.close()
-
+            if page % 20 == 0:
+                print(page, round(time() - start_time, 6))
 
 if __name__ == '__main__':
-    get_main_links(False)
+    get_main_links(filterSLO=True)
 
 
