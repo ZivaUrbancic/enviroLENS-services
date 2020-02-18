668c1becbe8ba674879a11b82e09f54a747ef000
KraljSamo <samokralj95@gmail.com>
2020-02-18 13:49:42 +0100

    document retrieval bugfix

--- document_retrieval/microservice/__init__.py
+++ document_retrieval/microservice/__init__.py
@@ -48,11 +48,8 @@ def create_app(args=None):
         config_logging.init_app(app)
 
         # add index routes
-        from .routes import index
+        from .routes import index, service
         app.register_blueprint(index.bp)
-
-        # add embedding routes
-        from .routes import service
         app.register_blueprint(service.bp)
 
     # TODO: log start of the service
